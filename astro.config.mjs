71a15f0c287043914471fe83930822a954357797
Tim <github@timgreen.ws>
2025-08-28 21:50:55 +1000

    added alpinejs/tailwind and outdir/build for subdomain to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,16 @@
+// @ts-check
 import { defineConfig } from "astro/config";
+import alpinejs from "@astrojs/alpinejs";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
 export default defineConfig({
+  integrations: [alpinejs()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  outDir: './craft',
+  build: {
+    assetsPrefix: '/craft'
+  },
 });
